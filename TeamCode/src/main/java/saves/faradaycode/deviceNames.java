fde40f1417af43b3444a9aa88f4a42753c0acbb2
pranavshankar111 <159638799+pranavshankar111@users.noreply.github.com>
2024-05-21 15:38:48 -0400

    Initial commit

---
+++ TeamCode/src/main/java/saves/faradaycode/deviceNames.java
@@ -0,0 +1,32 @@
+package saves.faradaycode;
+
+public interface deviceNames {
+
+    //servos
+    String dummyServoName = "dummyServo"; //Expansion hub 2
+
+    //motors
+    String dummyDCMotorName = "dummyDCMotor"; //Control hub 1
+
+    //crservos
+    String dummyCRServoName = "dummyCRServo";
+
+    // drivetrain
+    String leftFrontName = "leftFront";
+    String leftRearName = "leftRear";
+    String rightRearName = "rightRear";
+    String rightFrontName = "rightFront";
+
+    //encoders
+    String leftEncoderName = "slide2";
+    String rightEncoderName = "hang";
+    String frontEncoderName = "intakeA";
+
+    //tFod
+    String webCamName = "Webcam 1";
+    String blueLabel = "b";
+    String redLabel = "r";
+    String blueAsset = "bb.tflite";
+    String redAsset = "rr.tflite";
+
+}
