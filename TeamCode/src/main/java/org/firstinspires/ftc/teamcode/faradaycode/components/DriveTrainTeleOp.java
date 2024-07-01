072970f62b37cc42603382d8f219329c231c635c
YutYouGit <yutyoulook@outlook.com>
2024-07-01 17:12:45 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/faradaycode/components/DriveTrainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/faradaycode/components/DriveTrainTeleOp.java
@@ -6,7 +6,7 @@ import org.firstinspires.ftc.teamcode.faradaycode.*;
 public class DriveTrainTeleOp implements deviceNames{
 
     //constants
-    public double powerRange = 1;
+    public final double powerRange = 1;
     public double slowConst = 0.45;
 
     //inits motor objects
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/faradaycode/components/NerfSlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/faradaycode/components/NerfSlow.java
@@ -6,9 +6,11 @@ public class NerfSlow {
 
     //constants
     double nerfInc = 0.0001;
+    boolean isHeldDown = false;
 
     //iterates
     public void iterate(double slowVal, boolean increase, boolean decrease) {
+        isHeldDown = (increase || decrease);
         OpModes.nerf += (increase) ? nerfInc : ((decrease) ? -nerfInc : 0);
         OpModes.slowAmnt = 1 - slowVal;
         //OpModes.isSlow = slowVal >= 0.6;
